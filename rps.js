43327a9990108c4728e476031cdd17c5e6734649
Ajay Chabra <mets14@gmail.com>
2024-08-28 15:49:28 -0700

    ui added

--- rps.js
+++ rps.js
@@ -1,40 +1,25 @@
 
+let humanScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+
 function getComputerChoice() {
     let randomNum = Math.random();
     if (randomNum <= 1.0 / 3.0) {
-        return "rock";
+        return "Rock";
     }
     else if (randomNum <= 2.0 / 3.0) {
-        return "paper";
+        return "Paper";
     }
     else {
-        return "scissors";
+        return "Scissors";
     }
 }
-
-function getHumanChoice() {
-    let humanAction = prompt("Please select your action");
-
-    if (humanAction[0] == 'r') {
-        return "rock";
-    }
-    else if (humanAction[0] == 'p') {
-        return "paper";
-    }
-    else if (humanAction[0] == 's') {
-        return "scissors";
-    }
-    else {
-        alert("Invalid action, please enter either r, p or s");
-        return getHumanChoice();
-    }
-}
-
 
 
 // returns whether p1 wins vs p2 in a h2h
 function getWinner(p1Choice, p2Choice) {
-    const validChoices = ["rock", "paper", "scissors"];
+    const validChoices = ["Rock", "Paper", "Scissors"];
     
     if (!validChoices.includes(p1Choice)) {
         throw new Error("Invalid choice from p1: " + p1Choice);
@@ -47,51 +32,77 @@ function getWinner(p1Choice, p2Choice) {
     if (p1Choice == p2Choice)
         return "tie";
     switch (p1Choice) {
-        case "rock":
-            return p2Choice == "scissors" ? "win" : "loss";
-        case "paper":
-            return p2Choice == "rock" ? "win" : "loss";
-        case "scissors":
-            return p2Choice == "paper" ? "win" : "loss";
+        case "Rock":
+            return p2Choice == "Scissors" ? "win" : "loss";
+        case "Paper":
+            return p2Choice == "Rock" ? "win" : "loss";
+        case "Scissors":
+            return p2Choice == "Paper" ? "win" : "loss";
     }
 }
 
-
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        const result = getWinner(humanChoice, computerChoice);
-    
-        switch (result) {
-            case "win":
-                console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
-                ++humanScore;
-                break;
-            case "loss":
-                console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
-                ++computerScore;
-                break;
-            case "tie":
-            console.log(`You Tie! ${humanChoice} ties ${computerChoice}`);
-                break;
-        }
-    }
-
-    for (i = 0; i != 5; ++i) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    if (humanScore > computerScore)
-        console.log(`You Win! ${humanScore} - ${computerScore}`);
-    else if (humanScore == computerScore)
-        console.log(`Tie!  ${humanScore} - ${computerScore}`);
-    else
-        console.log(`You Lose!  ${computerScore} - ${humanScore}`);
+function updateResultsView(lastResultMsg) {
+    document.querySelector("#hero-score").textContent = `Hero Score : ${humanScore}`;
+    document.querySelector("#bot-score").textContent = `Bot Score : ${computerScore}`;
+    document.querySelector("#last-result").textContent = lastResultMsg;
 }
 
-playGame();
+function getLastResultMsg(humanChoice, computerChoice, result) {
+    switch (result) {
+        case "win":
+            return `You Win! ${humanChoice} beats ${computerChoice}`;
+        case "loss":
+            return `You Lose! ${computerChoice} beats ${humanChoice}`;
+        case "tie":
+            return `You Tie! ${humanChoice} ties ${computerChoice}`;
+    }
+}
+
+function playRound(humanChoice) {
+    const computerChoice = getComputerChoice();
+
+    let result = getWinner(humanChoice, computerChoice);
+
+    updateScore(result);
+    updateResultsView(getLastResultMsg(humanChoice, computerChoice, result));
+    updateIfOverallWinner();
+}
+
+function updateIfOverallWinner() {
+    function disableButtons() {
+        ["Rock", "Paper", "Scissors"].forEach(id => {
+            document.getElementById(id).setAttribute('disabled', true);
+        });
+    }
+
+    if (humanScore == 5) {
+        document.querySelector("#overall-result").textContent = "Congrats you won the game!";
+        disableButtons();
+    }
+    else if (computerScore == 5) {
+        document.querySelector("#overall-result").textContent = "Sorry you lost the game!";
+        disableButtons();
+    }
+}
+
+function updateScore(humanResult) {
+    switch (humanResult) {
+        case "win":
+            ++humanScore;
+            break;
+        case "loss":
+            ++computerScore;
+            break;
+        case "tie":
+            break;
+    }
+}
+
+updateResultsView();
+
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        playRound(btn.id);
+    })
+});
